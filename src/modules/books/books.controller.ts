d0cb0dc0c22c2261446430620b797f958af3cea3
Daniel Oliveira <kadadniel@gmail.com>
2026-03-25 13:59:13 -0300

    feat(books): implement PATCH /readings/:readingId in BooksController

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
 
 import { BooksService } from './books.service';
 
 import type { BookWithAuthorAndGenre } from './types/book-with-author-and-genre';
+import { ReadingIdParamDTO } from './dtos/reading-id.param.dto';
+import { ReadingsService } from '../readings/readings.service';
+import { UpdateReadingDataDTO } from './dtos/update-reading-data.dto';
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly booksService: BooksService) {}
+  constructor(
+    private readonly booksService: BooksService,
+    private readonly readingsService: ReadingsService,
+  ) {}
 
   @Get()
   listAllWithAuthorAndGenres(): Promise<BookWithAuthorAndGenre[]> {
     return this.booksService.listAllWithAuthorAndGenres();
+  }
+
+  @Patch('readings/:readingId')
+  updateReading(
+    @Param() param: ReadingIdParamDTO,
+    @Body() data: UpdateReadingDataDTO,
+  ) {
+    return this.readingsService.update({
+      id: param.readingId,
+      ...data,
+    });
   }
 }
--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ReadingsModule } from '../readings/readings.module';
 
 import { BooksService } from './books.service';
 
 import { BooksController } from './books.controller';
 
 @Module({
+  imports: [ReadingsModule],
   providers: [BooksService],
   controllers: [BooksController],
   exports: [BooksService],
